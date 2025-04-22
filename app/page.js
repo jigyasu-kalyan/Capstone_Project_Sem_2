224b32f336e0c8604aa732667fa6c11aee20cb4e
Private <jigyasukalyan1276016@gmail.com>
2025-04-22 19:31:01 +0530

    Initialized Next.js with Tailwind CSS

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="text-4xl text-center text-blue-500 mt-10">
+      Hello Health & Fitness! 🧘‍♂️💪
+    </div>    
+  );
+}
